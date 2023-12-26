67139261ba4883362b7ec95d3f3a77a35de6ad4d
UmmiKalsumTaqiah722 <154320480+UmmiKalsumTaqiah722@users.noreply.github.com>
2023-12-26 09:58:47 +0800

    Add files via upload

---
+++ taekwondo_registration.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 26, 2023 at 02:57 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `taekwondo_registration`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member_information`
+--
+
+CREATE TABLE `member_information` (
+  `Name` varchar(30) NOT NULL,
+  `ID_Number` int(20) NOT NULL,
+  `Gender` varchar(10) NOT NULL,
+  `Year_of_Born` int(10) NOT NULL,
+  `Age` int(10) NOT NULL,
+  `Discount` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `member_information`
+--
+
+INSERT INTO `member_information` (`Name`, `ID_Number`, `Gender`, `Year_of_Born`, `Age`, `Discount`) VALUES
+('Ummi Kalsum', 1, 'Female', 2004, 19, 50),
+('Mail', 2, 'Male', 2013, 10, 45),
+('umar', 3, 'Male', 2003, 20, 50);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
